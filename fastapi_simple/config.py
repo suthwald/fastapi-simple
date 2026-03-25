86f2370878077f299319bccf3d29d9671559a4a1
Dharmender Singh <dharmender071999@gmail.com>
2026-03-25 16:38:03 +0530

    fixed formatting

--- fastapi_simple/config.py
+++ fastapi_simple/config.py
@@ -1,9 +1,11 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
     APP_NAME: str = "FastAPI Simple"
 
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
